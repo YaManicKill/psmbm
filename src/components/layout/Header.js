f0d38f53d720a215a698978fd95c8f2507663cac
Al McKinlay <github@10people.co.uk>
2020-08-03 18:23:57 +0100

    Add virtually beach mission logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LOGO from '../../static/virtually-logo.jpg';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
-    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
+    <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-1 px-8">
       <div className="flex items-center">
-        <h1 className="text-3xl font-bold leading-none">Virtually Beach Mission</h1>
+        <img src={LOGO} className="h-16"></img>
+        <h1 className="text-3xl font-bold leading-none ml-5">Virtually Beach Mission</h1>
       </div>
     </div>
   </header>
