7c6b964aad8235863a82a39ab1ee967008bd7a33
Al McKinlay <github@10people.co.uk>
2020-08-03 19:00:43 +0100

    Make heading smaller on smaller devices

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,8 +5,10 @@ const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-1 px-8">
       <div className="flex items-center">
-        <img src={LOGO} className="h-16"></img>
-        <h1 className="text-3xl font-bold leading-none ml-5">Virtually Beach Mission</h1>
+        <img src={LOGO} className="h-12 sm:h-16"></img>
+        <h1 className="text-xl sm:text-2xl lg:text-3xl font-bold leading-none ml-5">
+          Virtually Beach Mission
+        </h1>
       </div>
     </div>
   </header>
