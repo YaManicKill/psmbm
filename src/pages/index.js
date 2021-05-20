3acc4ea719f7a1b32c5501c32e189d763f5bfd88
Al McKinlay <github@10people.co.uk>
2021-05-20 13:34:47 +0100

    Fix eslint issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,12 @@ import Layout from '../components/layout/Layout';
 
 import BM2020 from '../sections/2020';
 import BM2021 from '../sections/2021';
-import Contact from '../sections/contact';
 
-export default () => (
+const Index = () => (
   <Layout>
     <BM2021></BM2021>
     <BM2020></BM2020>
   </Layout>
 );
+
+export default Index;
