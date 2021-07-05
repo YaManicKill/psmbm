975bc9120c1e208f378f733804c5de6771120abd
Al McKinlay <github@10people.co.uk>
2021-07-05 09:47:08 +0100

    Update session times

--- src/sections/2021.js
+++ src/sections/2021.js
@@ -24,7 +24,7 @@ export default () => (
       className="pr-10"
       src={SESSION_TIMES}
       alt="Session times. 
-      Starfish, 0-4 year olds (with parent/carer) 10 till 11:50am and 2:30 till 4pm. 
+      Starfish, 0-4 year olds (with parent/carer) 2:30 till 4pm. 
       Turtles, 5-7 year olds, 10 till 11:50 am and 2:30 till 4pm.  
       Narwhals, 8-11 year olds, 10 till 11:50 am and 2:30 till 4pm.  
       Sharks, 12-17 year olds, 10 till 11:50 am, 2:30 till 4pm, and 7:30 till 9:30pm. 
