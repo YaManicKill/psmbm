51de34588542fcfc91367a156a159d6806108040
Al McKinlay <github@10people.co.uk>
2022-06-18 20:35:51 +0100

    Update 2022 info

--- src/sections/2022.js
+++ src/sections/2022.js
@@ -7,6 +7,12 @@ export default () => (
   <section id="2021">
     <Heading>Beach Mission 2022</Heading>
     <p className="text-2xl lg:text-3xl mt-6 font-light">31st July – 12th August</p>
-    <p className="text-l lg:text-xl mt-6 font-light">Beach Mission will return in 2022.</p>
+    <p className="text-l lg:text-xl mt-6 font-light">
+      Plans are well underway for this summer's Beach Mission when we will return to a full
+      programme of activities for children and young people aged 0-17. We expect to open up the
+      registration system early in July. If your child was registered last summer and you gave us
+      permission to contact you, you should receive an email to tell you when bookings have opened.
+      Otherwise, please just keep an eye on this website for further information.
+    </p>
   </section>
 );
