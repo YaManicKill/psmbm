3458074a6c37c6a6e4d089cad0230a7376378229
Al McKinlay <github@10people.co.uk>
2023-05-25 09:01:37 +0100

    Remove reference to registration link

--- src/sections/2022.js
+++ src/sections/2022.js
@@ -14,28 +14,24 @@ export default () => (
       </a>
     </p> */}
     <p className="text-l lg:text-xl mt-6 font-light">
-      Beach Mission will be back this summer, and all children and young people will be welcome to join in. There will be
-      activities every weekday for 0-17 year olds in six different age groups. Everyone is welcome,
-      and activities are free (apart from a contribution for the outing). Activities include games,
-      crafts, competitions, an outing, and interactive Bible based activities appropriate for each
-      age group. Information about how to register will be available in June,
+      Beach Mission will be back this summer, and all children and young people will be welcome to
+      join in. There will be activities every weekday for 0-17 year olds in six different age
+      groups. Everyone is welcome, and activities are free (apart from a contribution for the
+      outing). Activities include games, crafts, competitions, an outing, and interactive Bible
+      based activities appropriate for each age group. Information about how to register will be
+      available in June,
     </p>
     <p className="text-l lg:text-xl mt-6 font-light">
-      Children and young people are welcome to attend whichever sessions they wish. Check
-      out our age groups and session times here:
+      Children and young people are welcome to attend whichever sessions they wish. Check out our
+      age groups and session times here:
     </p>
     <br />
     <img src={SESSION_TIMES} />
     {/* <img src={FAMILY_EVENTS} /> */}
     <p className="text-l lg:text-xl mt-6 font-light">
       All children and young people must be registered for safeguarding reasons and to give us
-      contact details and other important information. Please book a place for your child(ren)
-      <a href="https://content.scriptureunion.org.uk/holiday/port-st-mary-su-beach-mission">
-        {' '}
-        at this link
-      </a>
-      . More information will be provided to families who register, or if you have any questions,
-      please use the contact form.
+      contact details and other important information. A link to the registration form will be
+      available in June. In the meantime, if you have any questions, please use the contact form.
     </p>
   </section>
 );
