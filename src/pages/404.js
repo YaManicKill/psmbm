68c4fbf9e590cbec941ed3e96426f89bf3654d20
Al McKinlay <github@10people.co.uk>
2023-07-11 16:08:42 +0100

    Add 404 page

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout/Layout'
+
+const FourOhFourPage = () => {
+	return (
+		<Layout>
+			<div className="container text-center mx-auto pt-10 text-xl">Page Not Found</div>
+		</Layout>
+	)
+}
+
+export default FourOhFourPage
