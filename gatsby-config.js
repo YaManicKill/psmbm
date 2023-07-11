c05b7bf8b36b2b503f8b0a911772f921b1298d3f
Al McKinlay <github@10people.co.uk>
2023-07-11 08:23:02 +0100

    Fix cms previews

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,11 @@ module.exports = {
       resolve: `gatsby-transformer-remark`,
       options: {},
     },
-    `gatsby-plugin-netlify-cms`
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/cms.js`,
+      },
+    },
   ]
 };
